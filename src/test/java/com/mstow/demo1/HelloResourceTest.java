104cd3fab4b4e4f20d8e78689212c4be6367c6f2
Andrzej Węgrzyn <andrzej9393@gmail.com>
2019-12-07 14:33:07 +0100

    @WebMvcTest

--- src/test/java/com/mstow/demo1/HelloResourceTest.java
+++ src/test/java/com/mstow/demo1/HelloResourceTest.java
@@ -4,7 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,7 +14,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
-@SpringBootTest
+@ContextConfiguration(classes=Demo1Application.class)
+@WebMvcTest({HelloResource.class, B.class, A.class})
 public class HelloResourceTest {
 
     @Autowired
