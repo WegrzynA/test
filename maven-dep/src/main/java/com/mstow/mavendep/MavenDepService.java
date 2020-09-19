4c1427be62c6e3b1f494edf921a0df917ba6b700
Andrzej Węgrzyn <andrzej9393@gmail.com>
2020-09-19 16:04:50 +0200

    maven assembly

---
+++ maven-dep/src/main/java/com/mstow/mavendep/MavenDepService.java
@@ -0,0 +1,7 @@
+package com.mstow.mavendep;
+
+public class MavenDepService {
+    public void print() {
+        System.out.println("dependency print");
+    }
+}
---
+++ maven/src/main/java/Main.java
@@ -0,0 +1,7 @@
+import com.mstow.mavendep.MavenDepService;
+
+public class Main {
+    public static void main(String[] args) {
+        new MavenDepService().print();
+    }
+}
